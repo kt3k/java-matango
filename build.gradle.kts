5279d56ab4eabe0e0a9b95f54827de9b58d4d156
Yoshiya Hinosawa <stibium121@gmail.com>
2019-05-19 19:39:04 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    api("org.apache.commons:commons-math3:3.6.1")
+    implementation("com.google.guava:guava:27.0.1-jre")
+    testImplementation("junit:junit:4.12")
+}
